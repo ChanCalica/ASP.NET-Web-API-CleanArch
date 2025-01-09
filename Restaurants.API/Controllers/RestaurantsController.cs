078f5d37068fa879417a10b78ffbdec0a52259b4
Christian Calica <calicachristianromero@gmail.com>
2025-01-10 07:08:54 +0800

    assigning user roles

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -7,6 +7,7 @@ using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Application.Restaurants.Queries.GetRestaurantById;
+using Restaurants.Domain.Constants;
 
 namespace Restaurants.API.Controllers;
 
@@ -84,6 +85,7 @@ public class RestaurantsController(/*IRestaurantsService restaurantsService*/ IM
     }
 
     [HttpPost]
+    [Authorize(Roles = UserRoles.Owner)]
     public async Task<IActionResult> CreateRestaurant(/*[FromBody] CreateRestaurantDto createRestaurantDto*/ CreateRestaurantCommand command)
     {
         //if (!ModelState.IsValid)
@@ -92,6 +94,8 @@ public class RestaurantsController(/*IRestaurantsService restaurantsService*/ IM
         //}
 
         //int Id = await restaurantsService.CreateRestaurant(createRestaurantDto);
+
+        //User.IsInRole();
 
         int Id = await mediator.Send(command);
 
